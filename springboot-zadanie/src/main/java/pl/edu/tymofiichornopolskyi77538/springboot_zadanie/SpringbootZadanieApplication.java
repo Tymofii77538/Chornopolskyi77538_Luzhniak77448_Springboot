10356f069b59e3b90e23e053ab1b0cad32abd094
Tymofii77538 <tchorno1@stu.vistula.edu.pl>
2026-05-12 19:19:49 +0200

    77538 spring boot aplikacja

---
+++ springboot-zadanie/src/main/java/pl/edu/tymofiichornopolskyi77538/springboot_zadanie/SpringbootZadanieApplication.java
@@ -0,0 +1,13 @@
+package pl.edu.tymofiichornopolskyi77538.springboot_zadanie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootZadanieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootZadanieApplication.class, args);
+	}
+
+}
---
+++ springboot-zadanie/src/test/java/pl/edu/tymofiichornopolskyi77538/springboot_zadanie/SpringbootZadanieApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.edu.tymofiichornopolskyi77538.springboot_zadanie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootZadanieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
